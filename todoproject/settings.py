2f5cbac8d2814f5b2b82257b0f522d03cf1a4ac5
jjanczynska <joaxjan@gmail.com>
2023-07-06 20:23:20 +0000

    add new key

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-il&l(%+vv=+j=-=6cyfi8*@xj8=8z4#hkq9ni(fqqkff6$su(0')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
