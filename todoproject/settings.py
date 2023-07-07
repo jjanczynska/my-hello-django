31b9a48889c6a53fc6859543bd1f7bddbb1c8b4b
jjanczynska <joaxjan@gmail.com>
2023-07-07 13:11:05 +0000

    update key

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'oli+0)aq@j(%e@169h7p8z*%=x4&d77wzn=dbg62juwtm0310q')
+SECRET_KEY = 'oli+0)aq@j(%e@169h7p8z*%=x4&d77wzn=dbg62juwtm0310q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
